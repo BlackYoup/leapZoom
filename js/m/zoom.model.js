e63e40245e610b768cad67fbc3ad650170b70d1c
Lefebvre Arnaud <a.lefebvre@outlook.fr>
2014-07-22 23:22:38 +0200

    Get index fingers position

--- js/m/zoom.model.js
+++ js/m/zoom.model.js
@@ -3,7 +3,11 @@ function ZoomModel(){
 
 	this.Leap = null;
 	this.waitForFrame = null;
-
+	this.currentFrame = null;
+	this.previousFrame = null;
+	this.currentIndexesFingersPos = null;
+	this.previousIndexesFingersPos = null;
+	
 	this.haveUpdate = function(){
 		emitter.emit('views:haveUpdate');
 	};
@@ -19,6 +23,54 @@ function ZoomModel(){
 
 	this.onFrame = function(frame){
 		this.initWaitForFrame();
+		this.previousFrame = this.currentFrame;
+		this.currentFrame = frame;
+		if(this.checkHands(2)){
+			this.defineIndexesPositions();
+		}
+	};
+
+	this.checkHands = function(nbr){
+		return this.currentFrame.hands.length === nbr;
+	};
+
+	this.getIndexFingers = function(){
+		var tempHands = _.map(this.currentFrame.hands, function(hand){
+			return {
+				type: hand.type, 
+				indexFinger: hand.indexFinger
+			};
+		});
+	
+		var fingers = {};
+		_.each(tempHands, function(hand){
+			if(!_.has(fingers, hand.type)){
+				fingers[hand.type] = hand.indexFinger;
+			}
+		});
+
+		return fingers;
+	};
+
+	this.getIndexFingersPos = function(fingers){
+		var myFingers = _.map(fingers, function(finger, key){
+			var myFingerData = _.filter(self.currentFrame.pointables, function(curFinger){
+				return curFinger.id === fingers[key].id;
+			});
+
+			return {
+				type: key,
+				fingerPos: myFingerData[0].tipPosition
+			};
+		});
+		return myFingers;
+	};
+
+	this.defineIndexesPositions = function(){
+		this.previousIndexesPos = this.currentIndexesFingersPos;
+		
+		var currentIndexFingers = this.getIndexFingers();
+		this.currentIndexFingersPos = this.getIndexFingersPos(currentIndexFingers);
 	};
 
 	this.initLeap = function(){
--- js/v/zoom.view.js
+++ js/v/zoom.view.js
@@ -13,6 +13,10 @@ function ZoomView(_model){
 			
 	};
 
+	this.wireDomEvents = function(){
+	
+	};
+
 	this.init = function(){
 		this.wireDomEvents();
 	};
